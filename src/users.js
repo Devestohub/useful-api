77ac44f183e577aa45f9da22bac71b15a710229a
Hugo Vidal <Hugo.vidal.ferre@gmail.com>
2020-05-04 19:58:11 +0200

    1.1.6

--- src/users.js
+++ src/users.js
@@ -21,14 +21,13 @@ class User {
 	 * @returns {void}
 	 */
 
-	connectDB() {
-		this.user.defaults({ info: {} }).write().then(() => {
-			this.user.set('info.id', this.user_id).write();
-			this.user.set('info.dname', this.user_name).write();
-			this.user.set('info.rol', "NonPlayer").write();
-			this.user.set('info.lang', "es").write();
-			this.user.set('info.avatarurl', this.user_avatarurl).write();
-		})
+	async connectDB() {
+		await this.user.defaults({ info: {} }).write();
+		this.user.set('info.id', this.user_id).write();
+		this.user.set('info.dname', this.user_name).write();
+		this.user.set('info.rol', "NonPlayer").write();
+		this.user.set('info.lang', "es").write();
+		this.user.set('info.avatarurl', this.user_avatarurl).write();
 	}
 
 	getPlayer(name) {
