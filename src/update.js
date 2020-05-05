8b7aef9867e531ea0206ada229c13d5fece37c7e
Hugo Vidal <Hugo.vidal.ferre@gmail.com>
2020-05-05 14:02:15 +0200

    4.2.0

--- src/update.js
+++ src/update.js
@@ -1,10 +1,27 @@
-const child_process = require('child_process')
+const { exec } = require('child_process')
 
-function cp() {
-    child_process.exec('npm i').then(() => console.log('✓ npm i'))
-    child_process.exec('npm update').then(() => console.log('✓ npm update'))
-    child_process.exec('npm i @hugovidafe/hugovidafe-db@latest').then(() => console.log('✓ npm i @hugovidafe/hugovidafe-db@latest'))
-    child_process.exec('nvm install latest').then(() => console.log('✓ nvm install latest'))
+function cp(param, callback) {
+    exec(param, (error, stdout, stderr) => {
+        callback = stdout;
+    })
 }
 
-module.exports = cp;
+function update() {
+    cp('npm i', cb => {
+        console.log(cb)
+    })
+    
+    cp('npm update', cb => {
+        console.log(cb)
+    })
+    
+    cp('npm i @hugovidafe/hugovidafe-db@latest', cb => {
+        console.log(cb)
+    })
+    
+    cp('nvm install latest', cb => {
+        console.log(cb)
+    })
+}
+
+module.exports = update;
