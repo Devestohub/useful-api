aab11aeb2f31ff95083737e9f65821787224bc95
Hugo Vidal <Hugo.vidal.ferre@gmail.com>
2020-05-05 10:03:38 +0200

    1.1.0

--- src/users.js
+++ src/users.js
@@ -8,39 +8,79 @@ const FileSync = require('lowdb/adapters/FileSync')
  */
 
 class User {
-	constructor(id, name, avatarurl) {
+	constructor(id) {
 		this.user_id = id;
-		this.user_name = name;
-		this.user_avatarurl = avatarurl;
 		this.players = low(new FileSync(`./src/database/users/users.json`));
 		this.user = low(new FileSync(`./src/database/users/${this.user_id}.json`));
 	}
+
+
+	/**
+	 * Get the id of the mentioned player.
+	 * @param {string} name Name of the player.
+	 * @returns {string} Returns the id of the player.
+	 * @example
+	 * database.getPlayer('Hugovidafe')
+	 *   .then(param => console.log(`The player "Hugovidafe" have this id: ${param}`)) // RESULT: The player "Hugovidafe" have this id: 324449297951096834
+	 *   .catch(console.error);
+	 */
 
 	getPlayer(name) {
 		return this.players.get(name).value()
 	}
 
+	/**
+	 * Set a new player.
+	 * @param {string} name Name of the new player.
+	 * @param {string} id ID of the new player.
+	 * @returns {void}
+	 * @example
+	 * database.setPlayer('Hugovidafe', '324449297951096834')
+	 */
+
 	setPlayer(name, id) {
 		return this.players.set(name, id).write()
 	}
+
+
+	/**
+	 * Get the info of the mentioned player.
+	 * @param {string} name Name of the player.
+	 * @param {string} param Info of the player to get.
+	 * @returns {string} Returns the info of the player mentioned.
+	 * @example
+	 * database.getPlayerInfo('Hugovidafe', 'rol')
+	 *   .then(param => console.log(`The player "Hugovidafe" have this rol: ${param}`)) // RESULT: The player "Hugovidafe" have this rol: "Developer"
+	 *   .catch(console.error);
+	 */
 
 	getPlayerInfo(name, param) {
 		return low(new FileSync(`./src/database/users/${this.getPlayer(name)}.json`)).get(param).value()
 	}
 
+
 	/**
-	 * Get the set value of the database user.
-	 * @param {string} param Set a set value to return
-	 * @returns {string} Returns the set value of the database user.
+	 * Get the information about yourself.
+	 * @param {string} param Information to obtain from oneself.
+	 * @returns {string} Returns the information of oneself.
 	 * @example
-	 * database.get('lang')
-	 *   .then(param => console.log(`Obtained set value of the database user: ${param}`))
+	 * database.getUserInfo('lang')
+	 *   .then(param => console.log(`You have the language set to: ${param}`)) // RESULT: You have the language set to: "English"
 	 *   .catch(console.error);
 	 */
 
 	getUserInfo(param) {
 		return this.user.get(param).value()
 	}
+
+	/**
+	 * Set the information about yourself.
+	 * @param {string} param Parameter to configure itself
+	 * @param {string} value Parameter value to configure itself
+	 * @returns {void}
+	 * @example
+	 * database.setUserInfo('lang', "English")
+	 */
 
 	setUserInfo(param, value) {
 		this.user.defaults({ info: {} }).write();
