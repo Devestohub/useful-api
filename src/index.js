6b9b48595d5aafc499c62f59c3fa4d05de44b442
Hugo Vidal <Hugo.vidal.ferre@gmail.com>
2020-05-07 23:20:04 +0200

    4.4.0

--- src/index.js
+++ src/index.js
@@ -4,6 +4,5 @@ module.exports = {
     Langs: require('./langs'),
     Roles: require('./roles'),
     Users: require('./users'),
-    Update: require('./update'),
     version: require('../package.json').version
 }
